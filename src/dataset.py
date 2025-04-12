ae4ad6c30c5a0cd30b680cbe9d4f2f8b7041e4fa
Akhil2k3 <mkakhil2003@gmail.com>
2025-04-12 15:33:10 +0530

    code updates
    Updated main.py, model.py, dataset.py, featurize.py, graph_utils.py

--- src/dataset.py
+++ src/dataset.py
@@ -1,1 +1,32 @@
-# Dataset functions go here
+import tensorflow as tf
+
+def prepare_batch(x_batch, y_batch):
+    """
+    Merges (sub)graphs of batch into a single global (disconnected) graph.
+    """
+    atom_features, bond_features, pair_indices = x_batch
+    num_atoms = atom_features.row_lengths()
+    num_bonds = bond_features.row_lengths()
+    molecule_indices = tf.range(len(num_atoms))
+    molecule_indicator = tf.repeat(molecule_indices, num_atoms)
+    
+    # Compute increment for global graph merging
+    gather_indices = tf.repeat(molecule_indices[:-1], num_bonds[1:])
+    increment = tf.cumsum(num_atoms[:-1])
+    increment = tf.pad(tf.gather(increment, gather_indices), [(num_bonds[0], 0)])
+    
+    pair_indices = pair_indices.merge_dims(outer_axis=0, inner_axis=1).to_tensor()
+    pair_indices = pair_indices + increment[:, tf.newaxis]
+    atom_features = atom_features.merge_dims(outer_axis=0, inner_axis=1).to_tensor()
+    bond_features = bond_features.merge_dims(outer_axis=0, inner_axis=1).to_tensor()
+    return (atom_features, bond_features, pair_indices, molecule_indicator), y_batch
+
+def MPNNDataset(X, y, batch_size=32, shuffle=False):
+    dataset = tf.data.Dataset.from_tensor_slices((X, y))
+    if shuffle:
+        dataset = dataset.shuffle(1024)
+    return (
+        dataset.batch(batch_size)
+               .map(prepare_batch, num_parallel_calls=tf.data.AUTOTUNE)
+               .prefetch(tf.data.AUTOTUNE)
+    )
--- src/graph_utils.py
+++ src/graph_utils.py
@@ -1,1 +1,64 @@
-# Graph conversion utils go here
+import numpy as np
+import tensorflow as tf
+from rdkit import Chem
+from rdkit.Chem import SanitizeFlags
+
+# Import featurizers from our module
+from src.featurizer import AtomFeaturizer, BondFeaturizer
+
+# Instantiate featurizers (you can also do this in main.py if you prefer)
+atom_featurizer = AtomFeaturizer(
+    allowable_sets={
+        "symbol": {"B", "Br", "C", "Ca", "Cl", "F", "H", "I", "N", "Na", "O", "P", "S"},
+        "n_valence": {0, 1, 2, 3, 4, 5, 6},
+        "n_hydrogens": {0, 1, 2, 3, 4},
+        "hybridization": {"s", "sp", "sp2", "sp3"},
+    }
+)
+
+bond_featurizer = BondFeaturizer(
+    allowable_sets={
+        "bond_type": {"single", "double", "triple", "aromatic"},
+        "conjugated": {True, False},
+    }
+)
+
+def molecule_from_smiles(smiles):
+    molecule = Chem.MolFromSmiles(smiles, sanitize=False)
+    flag = Chem.SanitizeMol(molecule, catchErrors=True)
+    if flag != SanitizeFlags.SANITIZE_NONE:
+        Chem.SanitizeMol(molecule, sanitizeOps=SanitizeFlags.SANITIZE_ALL ^ flag)
+    Chem.AssignStereochemistry(molecule, cleanIt=True, force=True)
+    return molecule
+
+def graph_from_molecule(molecule):
+    atom_features = []
+    bond_features = []
+    pair_indices = []
+    for atom in molecule.GetAtoms():
+        atom_features.append(atom_featurizer.encode(atom))
+        # Self-loop: add the atom itself
+        pair_indices.append([atom.GetIdx(), atom.GetIdx()])
+        bond_features.append(bond_featurizer.encode(None))
+        for neighbor in atom.GetNeighbors():
+            bond = molecule.GetBondBetweenAtoms(atom.GetIdx(), neighbor.GetIdx())
+            pair_indices.append([atom.GetIdx(), neighbor.GetIdx()])
+            bond_features.append(bond_featurizer.encode(bond))
+    return np.array(atom_features), np.array(bond_features), np.array(pair_indices)
+
+def graphs_from_smiles(smiles_list):
+    atom_features_list = []
+    bond_features_list = []
+    pair_indices_list = []
+    for smiles in smiles_list:
+        molecule = molecule_from_smiles(smiles)
+        atom_features, bond_features, pair_indices = graph_from_molecule(molecule)
+        atom_features_list.append(atom_features)
+        bond_features_list.append(bond_features)
+        pair_indices_list.append(pair_indices)
+    return (
+        tf.ragged.constant(atom_features_list, dtype=tf.float32),
+        tf.ragged.constant(bond_features_list, dtype=tf.float32),
+        tf.ragged.constant(pair_indices_list, dtype=tf.int64),
+    )
+
